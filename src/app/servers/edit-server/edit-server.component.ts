e064cca340ddea04a7a3d72e41912510cf0c312e
Naveen <naveenou@gmail.com>
2019-11-10 18:34:35 +0530

    extracting query params and fragment

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -8,20 +9,32 @@ import { ServersService } from '../servers.service';
   styleUrls: ['./edit-server.component.css']
 })
 export class EditServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number; name: string; status: string };
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    console.log(
+      'queryparams: allowEdit: ' +
+        this.activatedRoute.snapshot.queryParams['allowEdit']
+    );
+    console.log('fragment: ' + this.activatedRoute.snapshot.fragment);
+    this.activatedRoute.queryParams.subscribe();
+    this.activatedRoute.fragment.subscribe();
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
 
   onUpdateServer() {
-    this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.serversService.updateServer(this.server.id, {
+      name: this.serverName,
+      status: this.serverStatus
+    });
   }
-
 }
